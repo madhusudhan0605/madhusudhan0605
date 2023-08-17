7493fba8f6abb627604a80d7bb7ccf8c82e07ebd
Burra Madhusudhan Reddy <madhusudhanreddy.burra@gmail.com>
2023-08-17 08:42:17 +0530

    Update weight.py

--- weight.py
+++ weight.py
@@ -1,8 +1,10 @@
 convert = 0.45
-weight = int(input("Enter the Weight "))
-kilo_or_lbs = input("weight is in kilo grams or lbs ")
+weight = int(input("Enter the Weight:"))
+kilo_or_lbs = input("weight is in kilograms(K) or lbs(L):")
 if kilo_or_lbs.upper() == 'K':
     weight = weight / convert
 elif kilo_or_lbs.upper() == 'L':
     weight = weight * convert
-print(weight)
+else:
+    Print("Enter Valid value")
+print("Converted value:"weight)
