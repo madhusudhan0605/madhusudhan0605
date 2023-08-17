cbf7abcd5c80061fc4b54c8124c35b2a7e319087
unknown <madhusudhanreddy.burra@gmail.com>
2023-08-17 09:02:03 +0530

    Updated the code by using oops

--- weight.py
+++ weight.py
@@ -1,10 +1,16 @@
+def weight_converter(convert,weight):
+    kilo_or_lbs = input("weight is in kilograms(K) or lbs(L):")
+    if kilo_or_lbs.upper() == 'K':
+        weight = weight / convert
+        print("Converted value:",weight)
+    elif kilo_or_lbs.upper() == 'L':
+        weight = weight * convert
+        print("Converted value:",weight)
+    else:
+        print("Enter Valid value!!!")
+        weight_converter(convert,weight)
+
 convert = 0.45
 weight = int(input("Enter the Weight:"))
-kilo_or_lbs = input("weight is in kilograms(K) or lbs(L):")
-if kilo_or_lbs.upper() == 'K':
-    weight = weight / convert
-elif kilo_or_lbs.upper() == 'L':
-    weight = weight * convert
-else:
-    print("Enter Valid value")
-print("Converted value:",weight)
+weight_converter(convert,weight)
+
